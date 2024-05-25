6792e5cf3e93dee81a48eb5dd57e0b25d7683ac7
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-05-24 21:48:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DeafLearning
+
+This project is a research prototype. To start it, make sure you have the following tools installed on your machine:
+- Python / Python 3
+- Flask
+
+To run it, navigate to root directory and run `python app.py` or `python3 app.py`.
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    # Specify the correct path to your video file
+    video_path = 'videos/input_file.mp4'
+    return send_file(video_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
