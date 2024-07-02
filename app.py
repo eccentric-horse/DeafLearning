3ed733bcf2809fee1a2e7b52117f5ed19b3f9865
Sloth186 <rothpm24@gmail.com>
2024-07-02 00:06:12 -0400

    Added video and added 25 out of about 40 questions. They are not assigned to proper categories (transcript, emotion, movement) as we do not have this information yet.

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def questions() -> "html":
 @app.route('/video')
 def video():
     # Specify the correct path to your video file
-    video_path = 'videos/input_file.mp4'
+    video_path = 'videos/ARwCaption.mp4'
     return send_file(video_path, as_attachment=True)
 
 if __name__ == '__main__':
