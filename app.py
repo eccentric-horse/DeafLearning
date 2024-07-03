adf0601e6e95a5db08b4e95ac203727062ac1f5e
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-07-03 13:19:19 -0700

    remove video endpoint

--- app.py
+++ app.py
@@ -31,11 +31,5 @@ def get_questions():
 def questions() -> "html":
     return render_template('questions.html')
 
-@app.route('/video')
-def video():
-    # Specify the correct path to your video file
-    video_path = 'videos/ARwCaption.mp4'
-    return send_file(video_path, as_attachment=True)
-
 if __name__ == '__main__':
     app.run(debug=True)
